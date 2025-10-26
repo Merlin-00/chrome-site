dcfff702fd52226d8f2e27fe1dce6aa5338f22ef
Merlin-00 <merlinlubambo@gmail.com>
2025-10-26 21:50:40 +0200

    ajustement2

--- src/app/pages/home-sections/fast/fast.ts
+++ src/app/pages/home-sections/fast/fast.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { IS_MEDIUM } from '../../../app.constants';
 import { WindowsObserver } from '../../../core/services/utilities/windows-observer';
 
 @Component({
   selector: 'app-fast',
-  imports: [MatIconModule, MatCardModule],
+  imports: [MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './fast.html',
   styleUrl: './fast.scss',
 })
@@ -14,4 +15,24 @@ export class Fast {
   private windows = inject(WindowsObserver);
   width = this.windows.width;
   medium = IS_MEDIUM;
+
+  currentIndex = 0;
+  cards = [0, 1, 2];
+
+  nextCard() {
+    if (this.currentIndex < this.cards.length - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  prevCard() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  getTransform() {
+    const offset = this.currentIndex * (900 + 48 - 150);
+    return `translateX(-${offset}px)`;
+  }
 }
--- src/app/pages/home-sections/to-you/to-you.ts
+++ src/app/pages/home-sections/to-you/to-you.ts
@@ -1,7 +1,10 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, inject, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { IS_MEDIUM } from '../../../app.constants';
+import { WindowsObserver } from '../../../core/services/utilities/windows-observer';
+import { link } from 'fs';
 
 @Component({
   selector: 'app-to-you',
@@ -10,32 +13,36 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './to-you.scss',
 })
 export class ToYou implements OnInit {
+  private windows = inject(WindowsObserver);
+  width = this.windows.width;
+  medium = IS_MEDIUM;
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   activeIndex = 0;
   fillHeight = 0;
 
-  images = [
-    '/assets/sections-images/to-you/image1.png',
-    'assets/sections-images/image3.png',
-    'assets/sections-images/to-you/image3.png',
-  ];
-
   steps = [
     {
+      image: '/assets/sections-images/to-you/image1.png',
       title: 'Personnalisez Chrome',
       text: `Personnalisez votre navigateur Web grâce à des thèmes, au mode sombre
         ainsi qu'à d'autres options conçues juste pour vous.`,
+      link: 'https://chrome.google.com/webstore/category/themes',
+      textLink: 'Découvrir les thèmes',
     },
     {
+      image: 'assets/sections-images/to-you/image2.png',
       title: 'Naviguez sur tous les appareils',
       text: `Connectez-vous à Chrome sur l'appareil de votre choix pour accéder à
         vos favoris, mots de passe enregistrés et bien plus encore.`,
     },
     {
+      image: 'assets/sections-images/to-you/image3.png',
       title: 'Gagnez du temps avec la saisie automatique',
       text: `Enregistrez des adresses, des mots de passe et bien plus dans Chrome
         pour la saisie automatique de vos informations.`,
+      link: 'http://accounts.google.com/',
+      textLink: 'Pour commencer, veuillez vous connecter',
     },
   ];
 
